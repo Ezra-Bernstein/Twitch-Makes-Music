4e7ae4c869c0f5a2e7411e7f2b338b4ac72d5c43
Ezra Bernstein <mremdb@gmail.com>
2021-01-15 00:17:01 -0500

    Added unique parts, instruments
    Added the ability for each user to join in their own part using join comand
    Alternatively, they can join with an instrument using join,<instrument> command. only tonal (non-percusssive) instruments are supported

--- stream.py
+++ stream.py
@@ -12,9 +12,10 @@ OAUTH_TOKEN = os.getenv("OAUTH_TOKEN")
 
 
 helix = twitch.Helix(CLIENT_ID, CLIENT_SECRET)
-score = stream.Stream()
+score = stream.Score()
 numNotes = 1
-# composers = []
+composers = []
+
 
 STREAMER_NAME="ezra0110"
 
@@ -24,6 +25,7 @@ twitch.Chat(channel='#'+STREAMER_NAME, nickname=STREAMER_NAME, oauth=OAUTH_TOKEN
 def message_handler(message):
     # global composers
     global score
+    global composers
     message_text = message.text
     message_sender = message.sender
     print(message_sender, message_text)
@@ -31,7 +33,24 @@ def message_handler(message):
     lastNote=None
     # print(arr)
     try:
-        if message_text == "stop" and message_sender == STREAMER_NAME:
+        if len(arr) == 2 and arr[0] != '' and arr[1] != '' and arr[0] == "join":
+            if message_sender not in composers:
+                part = stream.Part()
+                part.insert(0, instrument.fromString(arr[1]))
+                part.id = message_sender
+                part.partName = message_sender
+                score.insert(0,part)
+                print(part)
+                composers.append(message_sender)
+        elif message_text == "join":
+            if message_sender not in composers:
+                part = stream.Part()
+                part.id = message_sender
+                part.partName = message_sender
+                score.insert(0,part)
+                print(part)
+                composers.append(message_sender)
+        elif message_text == "stop" and message_sender == STREAMER_NAME:
             # score.metadata.composer = "Ezra0110"
             # for composer in composers:
             #     score.metadata.composer = score.metadata.composer + ", " + composer
@@ -40,10 +59,10 @@ def message_handler(message):
         elif message_text == "reset" and message_sender == STREAMER_NAME:
             score.clear()
             score.write('musicxml.png', 'musicxml.png')
-        elif len(arr) == 2 and arr[0] != '' and arr[1] != '':
+        elif len(arr) == 2 and arr[0] != '' and arr[1] != '' and message_sender in composers:
             #lastNote=None
             if arr[0] == "key" and re.match("^-?\d+$", arr[1]):
-                score.append(key.KeySignature(int(arr[1])))
+                score.parts.stream()[message_sender].append(key.KeySignature(int(arr[1])))
                 score.write('musicxml.png', 'musicxml.png')
             else:
 
@@ -54,22 +73,22 @@ def message_handler(message):
                 #note_octave = arr[1]
                 note_duration = float(arr[1]) if re.match("^(\d*\.\d+|\d+\.\d*|\d+)$",arr[1]) else arr[1]
                 # add_composer(message_sender)
-                lastNote = add_to_score(score, note_name, note_duration)# note_octave,)
+                lastNote = add_to_score(score, note_name, note_duration, message_sender)# note_octave,)
                 score.write('musicxml.png', 'musicxml.png')
     except Exception as e:
         print(e)
     #   print("yikes")
         if lastNote != None:
-            stream=score.notesAndRests 
+            #stream=score.notesAndRests 
             # last=stream.getElementById(stream.streamLength-1) 
             score.remove(lastNote, shiftOffsets=True)
         
 
 
 
-def add_to_score(score, note_name, note_duration): # note_octave,
+def add_to_score(score, note_name, note_duration, message_sender): # note_octave,
     global numNotes
-    
+    global composers
     numNotes += 1
     n = note.Note(note_name)# + note_octave)
     n.duration = duration.Duration(note_duration)
@@ -77,10 +96,10 @@ def add_to_score(score, note_name, note_duration): # note_octave,
     # print(n.name)
     # print(n.duration)
     last = n
-    score.append(n)
+    score.parts.stream()[message_sender].append(n)
+    # score.append(n)
     return last
     
-# def add_composer(message_sender):
-#     global composers
+# def add_composer(message_sender, composers):
 #     if message_sender not in composers:
 #         composers.append(message_sender)
